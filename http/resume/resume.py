ce123d7d84614c306d2f1a2350f64bc5accd58ee
ahelloworld <1165818439@qq.com>
2016-06-28 03:21:14 +0800

    fix

--- http/resume/resume.py
+++ http/resume/resume.py
@@ -15,6 +15,7 @@ def contenttype(path):
 		return 'audio/ogg'
 	return 'application/octet-stream'
 
+'''
 def readfile(dirn, resdict, slen):
 	for parent, dnames, fnames in os.walk(dirn):
 		for dname in dnames:
@@ -41,7 +42,38 @@ def make404(resdict):
 	data += str(len(text)) + '\r\nContent-Type: text/html;charset=utf-8\r\n\r\n'
 	data += text
 	resdict['404'] = data
+'''
 
+def make404():
+	text = '<p>Page Not Found</p>'
+	data = 'HTTP/1.1 404 NotFound\r\nServer: ResumeServer\r\nConnection: keep-alive\r\nContent-Length: '
+	data += str(len(text)) + '\r\nContent-Type: text/html;charset=utf-8\r\n\r\n'
+	data += text
+	return data
+
+def response(req, cookie):
+	global home_dir
+	fpath = req
+	if fpath.find('..') != -1:
+		return make404(), 404
+	try:
+		f = open(home_dir + '/mailbox' + fpath, 'r')
+		data = f.read()
+		f.close()
+		zbuf = StringIO.StringIO()
+		gziper = gzip.GzipFile(mode = 'wb', compresslevel = 9, fileobj = zbuf)
+		gziper.write(data)
+		gziper.close()
+		text = zbuf.getvalue()
+		data = 'HTTP/1.1 200 OK\r\nServer: ResumeServer\r\nConnection: keep-alive\r\nContent-Encoding: gzip\r\nContent-Length: '
+		data += str(len(text)) + '\r\n'
+		data += 'Content-Type: %s\r\n\r\n' % contenttype(fpath)
+		data += text
+		return data, 200
+	except:
+		return make404(), 404
+
+'''
 def response(req, cookie):
 	global resdict
 	if req != None and resdict.has_key(req):
@@ -57,3 +89,4 @@ make404(resdict)
 readfile(home_dir, resdict, len(home_dir))
 for x, y in resdict.items():
 	print x
+'''
